74351f9e89ca6440bb98674a02bd6c56618bb4d1
Thanh Le <lesythanh063@gmail.com>
2021-07-06 13:31:22 +0700

    add delete function

---
+++ resources/lang/en/mess.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'tasklist' => 'Task List',
+    'newtask' => 'New Task',
+    'task' => 'Task',
+    'addtask' => 'Add Task',
+    'currenttask' => 'Current Task',
+    'task1' => 'Task',
+    'delete' => 'DELETE',
+    'errorsview' => 'Whoops! Something went wrong!',
+    'create' => 'To create successfully',
+]; 
+?>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+
+        <!-- CSS And JavaScript -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"  crossorigin="anonymous">
+    <base href="{{ asset('')}}">
+    <link rel="stylesheet" href="asset/css/style.css">
+    </head>
+    <body>
+<div class="container">
+
+    @yield('content')
+</div>
+    </body>
+</html>
